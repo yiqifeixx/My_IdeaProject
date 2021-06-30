1a44c80299c7aa68a26cbda8c9997523f3370d0e
zhangsan <123@qq.com>
2021-06-30 22:18:13 +0800

    提交demo3到本地仓库

---
+++ Demo3/src/com/itheima/demo01.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+import java.util.ArrayList;
+
+public class demo01 {
+
+    public static void main(String[] args) {
+
+        /*ArrayList<String> list = new ArrayList<>();
+
+        list.add("张三");
+        list.add("张小明");
+
+        list.stream().filter(name->name.startsWith("张"))
+                .filter(name->name.length()==3)
+                .forEach(name-> System.out.println(name));*/
+
+
+    }
+}
