9bf63343e121e7a5f53efd4d80b9ded26eaf2493
zhangsan <123@qq.com>
2021-06-30 22:40:46 +0800

    demo3修改

--- Demo3/src/com/itheima/demo01.java
+++ Demo3/src/com/itheima/demo01.java
@@ -6,14 +6,15 @@ public class demo01 {
 
     public static void main(String[] args) {
 
-        /*ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
 
         list.add("张三");
         list.add("张小明");
 
         list.stream().filter(name->name.startsWith("张"))
                 .filter(name->name.length()==3)
-                .forEach(name-> System.out.println(name));*/
+                .forEach(name-> System.out.println(name));
+        System.out.println("hehe");
 
 
     }
